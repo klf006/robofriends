740d64d89a306cc197c69c2124da99c6eb7d78e8
klf006 <49726847+klf006@users.noreply.github.com>
2020-09-03 13:03:30 -0500

    added redux-logger, middleware

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -30,8 +30,8 @@ class App extends Component {
 
     componentDidMount() {
         fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(users => this.setState({robots: users}));        
+            .then(response => response.json())
+            .then(users => this.setState({robots: users}));        
     }
 
 
